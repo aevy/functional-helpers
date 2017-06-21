28cdded17ce9c63578b1034ebbd806622340550c
Anton Strömkvist <hi@ahst.ro>
2017-06-21 11:04:00 +0200

    Format with prettier

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import R from "ramda"
+import R from "ramda";
 
 export const renameKeys = R.curry((keysMap, obj) =>
   R.reduce(
@@ -6,64 +6,63 @@ export const renameKeys = R.curry((keysMap, obj) =>
     {},
     R.keys(obj)
   )
-)
+);
 
-export const pickDeep = R.curry((paths, obj) => (
+export const pickDeep = R.curry((paths, obj) =>
   R.pipe(
     R.map(
-      R.pipe(
-        R.split("."),
-        pathArr => ({ [R.last(pathArr)]: R.path(pathArr, obj) })
-      )
+      R.pipe(R.split("."), pathArr => ({
+        [R.last(pathArr)]: R.path(pathArr, obj)
+      }))
     ),
     R.mergeAll
   )(paths)
-))
+);
 
 export const pickTree = R.curry(function _pickTree(tree, src) {
   if (Array.isArray(tree)) {
-    return R.pick(tree, src) // If Array, use Ramda's `pick`
+    return R.pick(tree, src); // If Array, use Ramda's `pick`
   }
 
-  const result = {}
+  const result = {};
   for (let branch in tree) {
-    const srcNode = src[branch]
-    const treeNode = tree[branch]
+    const srcNode = src[branch];
+    const treeNode = tree[branch];
     if (srcNode === undefined || !treeNode) {
-      continue
+      continue;
     }
     result[branch] = treeNode instanceof Object
       ? _pickTree(treeNode, srcNode) // If Object, recurse
-      : srcNode // If anything else, copy src value
+      : srcNode; // If anything else, copy src value
   }
-  return result
-})
+  return result;
+});
 
-export const compactObj = R.pickBy(Boolean)
+export const compactObj = R.pickBy(Boolean);
 
 /**
  * Same as R.evolve, except it runs all tranformations,
  * defaulting the value to undefined if the key doesn"t exist on object
  **/
 export const evolveAll = R.curry(function _evolveAll(transformations, object) {
-  const result = {}
-  let transformation, key, type
+  const result = {};
+  let transformation, key, type;
   for (key in transformations) {
-    transformation = transformations[key]
-    type = typeof transformation
+    transformation = transformations[key];
+    type = typeof transformation;
     result[key] = type === "function"
       ? transformation(object[key])
       : transformation && type === "object"
         ? _evolveAll(transformation, object[key])
-        : object[key]
+        : object[key];
   }
-  return R.merge(object, result)
-})
+  return R.merge(object, result);
+});
 
-export const mergeBy = R.curry((one, two, obj) => R.merge(one(obj), two(obj)))
+export const mergeBy = R.curry((one, two, obj) => R.merge(one(obj), two(obj)));
 
-export const spread = R.converge(R.merge, [R.dissoc, R.propOr({})])
+export const spread = R.converge(R.merge, [R.dissoc, R.propOr({})]);
 
-export const flattenProp = spread
+export const flattenProp = spread;
 
-export const nilOrEmpty = R.either(R.isNil, R.isEmpty)
+export const nilOrEmpty = R.either(R.isNil, R.isEmpty);
--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-const R = require("ramda")
-const { expect } = require("chai")
+const R = require("ramda");
+const { expect } = require("chai");
 const {
   flattenProp,
   compactObj,
@@ -9,141 +9,151 @@ const {
   pickDeep,
   pickTree,
   mergeBy,
-  spread,
-} = require("./")
+  spread
+} = require("./");
 
 describe("functional helpers", () => {
   describe("renameKeys", () => {
     it("renames a key in an object", () => {
-      const source = { foo: "bar" }
-      expect(renameKeys({ foo: "hello"}, source))
-        .to.have.property("hello")
-        .and.not.have.property("foo")
-    })
+      const source = { foo: "bar" };
+      expect(renameKeys({ foo: "hello" }, source)).to.have
+        .property("hello")
+        .and.not.have.property("foo");
+    });
 
     it("doesn't change the value of the renamed key", () => {
-      const source = { foo: "bar" }
-      expect(renameKeys({ foo: "hello"}, source))
-        .to.have.property("hello", source.foo)
-    })
+      const source = { foo: "bar" };
+      expect(renameKeys({ foo: "hello" }, source)).to.have.property(
+        "hello",
+        source.foo
+      );
+    });
 
     it("doesn't touch other keys", () => {
       const source = {
         foo: "bar",
-        oob: "lel",
-      }
-      expect(renameKeys({ foo: "hello"}, source))
-        .to.have.property("oob", source.oob)
-    })
-
-  })
+        oob: "lel"
+      };
+      expect(renameKeys({ foo: "hello" }, source)).to.have.property(
+        "oob",
+        source.oob
+      );
+    });
+  });
 
   describe("pickDeep", () => {
     const source = {
       foo: {
         bar: {
           x: 1,
-          y: 2,
+          y: 2
         }
       },
       oob: {
         far: {
-          lol: "yeah",
-        },
-      },
-    }
+          lol: "yeah"
+        }
+      }
+    };
 
     it("picks a deep property and outputs an object with the same key and value as the property picked", () => {
-      expect(pickDeep(["foo.bar"], source)).to.deep.equal({ bar: source.foo.bar })
-    })
+      expect(pickDeep(["foo.bar"], source)).to.deep.equal({
+        bar: source.foo.bar
+      });
+    });
 
     it("takes multiple properties as input", () => {
       expect(pickDeep(["foo.bar.x", "oob.far"], source)).to.deep.equal({
         x: source.foo.bar.x,
         far: source.oob.far
-      })
-    })
-
-  })
+      });
+    });
+  });
 
   describe("pickTree", () => {
     it("returns an empty object if input object doesn't have any keys", () => {
-      expect(pickTree({}, {})).to.deep.equal({})
-      expect(pickTree({}, { foo: 1 })).to.deep.equal({})
-      expect(pickTree({}, { foo: { foo: 1, bar: "foo" } })).to.deep.equal({})
-    })
+      expect(pickTree({}, {})).to.deep.equal({});
+      expect(pickTree({}, { foo: 1 })).to.deep.equal({});
+      expect(pickTree({}, { foo: { foo: 1, bar: "foo" } })).to.deep.equal({});
+    });
 
     it("returns a shallow copy with all keys that exists on input, if they exist on source", () => {
-      expect(pickTree({ foo: true }, {})).to.deep.equal({})
-      expect(pickTree({ foo: true }, { foo: 1 })).to.deep.equal({ foo: 1 })
-      expect(pickTree({ foo: true }, { foo: { foo: 1, bar: "foo" } }))
-        .to.deep.equal({ foo: { foo: 1, bar: "foo" } })
-      expect(pickTree({ bar: true }, { bar: "lel", foo: { foo: 1, bar: "foo" } }))
-        .to.deep.equal({ bar: "lel" })
-    })
+      expect(pickTree({ foo: true }, {})).to.deep.equal({});
+      expect(pickTree({ foo: true }, { foo: 1 })).to.deep.equal({ foo: 1 });
+      expect(
+        pickTree({ foo: true }, { foo: { foo: 1, bar: "foo" } })
+      ).to.deep.equal({ foo: { foo: 1, bar: "foo" } });
+      expect(
+        pickTree({ bar: true }, { bar: "lel", foo: { foo: 1, bar: "foo" } })
+      ).to.deep.equal({ bar: "lel" });
+    });
 
     it("handles an array on the input tree as R.pick does", () => {
-      expect(pickTree(
-        {
-          foo: ["foo", "bar"],
-        },
-        {
-          foo: {
-            foo: 1,
-            bar: "foo",
-            oob: null,
-            far: 123,
+      expect(
+        pickTree(
+          {
+            foo: ["foo", "bar"]
           },
+          {
+            foo: {
+              foo: 1,
+              bar: "foo",
+              oob: null,
+              far: 123
+            }
+          }
+        )
+      ).to.deep.equal({
+        foo: {
+          foo: 1,
+          bar: "foo"
         }
-      )).to.deep.equal(
-        {
-          foo: {
-            foo: 1,
-            bar: "foo",
-          },
-        }
-      )
-    })
+      });
+    });
 
     it("picks deeply nested values", () => {
-      expect(pickTree(
-        {
-          foo: {
-            bar: {
-              oob: true,
-            },
+      expect(
+        pickTree(
+          {
+            foo: {
+              bar: {
+                oob: true
+              }
+            }
           },
-        },
-        {
-          foo: {
-            foo: 1,
-            bar: {
-              oob: "asdf",
-              far: 123,
-            },
-            oob: null,
-            far: 123,
-          },
+          {
+            foo: {
+              foo: 1,
+              bar: {
+                oob: "asdf",
+                far: 123
+              },
+              oob: null,
+              far: 123
+            }
+          }
+        )
+      ).to.deep.equal({
+        foo: {
+          bar: {
+            oob: "asdf"
+          }
         }
-      )).to.deep.equal(
-        {
-          foo: {
-            bar: {
-              oob: "asdf",
-            },
-          },
-        }
-      )
-    })
+      });
+    });
 
     it("works exactly like R.pick if passed just an array", () => {
-      expect(pickTree(["foo", "bar"], { foo: 1, bar: 2, oob: 3 })).to.deep.equal({ foo: 1, bar: 2 })
-    })
+      expect(
+        pickTree(["foo", "bar"], { foo: 1, bar: 2, oob: 3 })
+      ).to.deep.equal({ foo: 1, bar: 2 });
+    });
 
     it("ignores keys on input with falsey values", () => {
-      expect(pickTree({ foo: false, bar: true }, { foo: 1, bar: 2, oob: 3 })).to.deep.equal({ bar: 2 })
-    })
-  })
+      expect(
+        pickTree({ foo: false, bar: true }, { foo: 1, bar: 2, oob: 3 })
+      ).to.deep.equal({ bar: 2 });
+    });
+  });
 
   describe("compactObj", () => {
     it("removes properties with falsy values from an object", () => {
@@ -154,115 +164,120 @@ describe("functional helpers", () => {
         zero: 0,
         nan: NaN,
         nul: null
-      }
-      expect(compactObj(falsyProps)).to.deep.equal({})
-      expect(compactObj(falsyProps)).to.be.empty
-    })
+      };
+      expect(compactObj(falsyProps)).to.deep.equal({});
+      expect(compactObj(falsyProps)).to.be.empty;
+    });
 
     it("does not remove properties with truthy values from an object", () => {
       const truthyProps = {
         nonzero: 1,
         string: "bar",
         object: {},
-        symbol: Symbol(),
-      }
-      expect(compactObj(truthyProps)).to.deep.equal(truthyProps)
-    })
-  })
+        symbol: Symbol()
+      };
+      expect(compactObj(truthyProps)).to.deep.equal(truthyProps);
+    });
+  });
 
   describe("evolveAll", () => {
     it("works as R.evolve if all transformations exist on object", () => {
       const source = {
         firstName: "  Tomato ",
-        id: 123,
-      }
+        id: 123
+      };
       const transformations = {
         firstName: R.trim,
-        id: R.inc,
-      }
+        id: R.inc
+      };
       const result = {
         firstName: "Tomato",
-        id: 124,
-      }
-      expect(evolveAll(transformations, source)).to.deep.equal(result)
-    })
+        id: 124
+      };
+      expect(evolveAll(transformations, source)).to.deep.equal(result);
+    });
 
     it("runs transformation with `undefined` if it doesn't exist on object", () => {
       const source = {
         firstName: "  Tomato ",
-        id: 123,
-      }
+        id: 123
+      };
       const transformations = {
         firstName: R.trim,
         lastName: R.identity,
-        id: R.inc,
-      }
+        id: R.inc
+      };
       const result = {
         firstName: "Tomato",
         lastName: undefined,
-        id: 124,
-      }
-      expect(evolveAll(transformations, source)).to.deep.equal(result)
-    })
+        id: 124
+      };
+      expect(evolveAll(transformations, source)).to.deep.equal(result);
+    });
 
     it("carries over key/values if key doesn't exist on transformations", () => {
       const source = {
         firstName: "  Tomato ",
-        id: 123,
-      }
+        id: 123
+      };
       const transformations = {
         firstName: R.trim,
-        lastName: R.identity,
-      }
+        lastName: R.identity
+      };
       const result = {
         firstName: "Tomato",
         lastName: undefined,
-        id: 123,
-      }
-      expect(evolveAll(transformations, source)).to.deep.equal(result)
-    })
-
-  })
+        id: 123
+      };
+      expect(evolveAll(transformations, source)).to.deep.equal(result);
+    });
+  });
 
   describe("mergeBy", () => {
     it("takes two functions, applies each one to obj, and merges the resulting object", () => {
-      const obj = { foo: "bar", bar: { oob: "foo", far: "bar" } }
-      expect(mergeBy(R.identity, R.identity, obj)).to.deep.equal(obj)
-      expect(mergeBy(R.omit(["foo"]), R.omit(["bar"]), obj)).to.deep.equal(obj)
-      expect(mergeBy(R.omit(["bar"]), R.prop(["bar"]), obj))
-        .to.deep.equal({ foo: "bar", oob: "foo", far: "bar" })
-      expect(mergeBy(R.omit(["bar"]), R.always({ lel: "wat" }), obj))
-        .to.deep.equal({ foo: "bar", lel: "wat" })
-    })
-
-  })
-
+      const obj = { foo: "bar", bar: { oob: "foo", far: "bar" } };
+      expect(mergeBy(R.identity, R.identity, obj)).to.deep.equal(obj);
+      expect(mergeBy(R.omit(["foo"]), R.omit(["bar"]), obj)).to.deep.equal(obj);
+      expect(mergeBy(R.omit(["bar"]), R.prop(["bar"]), obj)).to.deep.equal({
+        foo: "bar",
+        oob: "foo",
+        far: "bar"
+      });
+      expect(
+        mergeBy(R.omit(["bar"]), R.always({ lel: "wat" }), obj)
+      ).to.deep.equal({ foo: "bar", lel: "wat" });
+    });
+  });
 
   describe("spread/flattenProp", () => {
     it("merges a prop object onto the parent object", () => {
-      expect(spread("bar", { foo: "bar", bar: { oob: "foo", far: "bar" } }))
-        .to.deep.equal({ foo: "bar", oob: "foo", far: "bar" })
-      expect(flattenProp("bar", { foo: "bar", bar: { oob: "foo", far: "bar" } }))
-        .to.deep.equal({ foo: "bar", oob: "foo", far: "bar" })
-    })
+      expect(
+        spread("bar", { foo: "bar", bar: { oob: "foo", far: "bar" } })
+      ).to.deep.equal({ foo: "bar", oob: "foo", far: "bar" });
+      expect(
+        flattenProp("bar", { foo: "bar", bar: { oob: "foo", far: "bar" } })
+      ).to.deep.equal({ foo: "bar", oob: "foo", far: "bar" });
+    });
 
     it("overwrites values on the parent object", () => {
-      expect(spread("bar", { foo: "bar", bar: { foo: "foo" } }))
-        .to.deep.equal({ foo: "foo" })
-      expect(flattenProp("bar", { foo: "bar", bar: { foo: "foo" } }))
-        .to.deep.equal({ foo: "foo" })
-    })
-  })
+      expect(spread("bar", { foo: "bar", bar: { foo: "foo" } })).to.deep.equal({
+        foo: "foo"
+      });
+      expect(
+        flattenProp("bar", { foo: "bar", bar: { foo: "foo" } })
+      ).to.deep.equal({ foo: "foo" });
+    });
+  });
 
   describe("nilOrEmpty", () => {
     it("returns true for empty or nil objects", () => {
-      const truthyValues = ["", null, undefined, {}, []]
-      truthyValues.forEach(value => expect(nilOrEmpty(value)).to.equal(true))
-    })
+      const truthyValues = ["", null, undefined, {}, []];
+      truthyValues.forEach(value => expect(nilOrEmpty(value)).to.equal(true));
+    });
 
     it("returns false for non-empty or non-nil objects", () => {
-      const falseyValues = ["foo", 123, { foo: "bar" }, ["foo"], 0]
-      falseyValues.forEach(value => expect(nilOrEmpty(value)).to.equal(false))
-    })
-  })
-})
+      const falseyValues = ["foo", 123, { foo: "bar" }, ["foo"], 0];
+      falseyValues.forEach(value => expect(nilOrEmpty(value)).to.equal(false));
+    });
+  });
+});
